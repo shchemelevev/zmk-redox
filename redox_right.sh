4a85cbceb2039210487229b26bb471de947a38cd
e_shchemelev <e_shchemelev@wargaming.net>
2023-12-14 19:31:26 +0200

    fix

---
+++ redox_right.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+rm *.uf2
+rm *.zip
+wget https://nightly.link/shchemelevev/zmk-redox/workflows/build/master/firmware.zip 
+unzip firmware.zip
+cp redox_right-nice_nano_v2-zmk.uf2 /Volumes/NICENANO/
+rm *.uf2
+rm *.zip
